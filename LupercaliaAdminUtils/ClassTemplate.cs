91015d8ac2d889d5a90e70cb5ceada146d678cf2
faketuna <faketuna.st@gmail.com>
2025-04-03 23:46:03 +0900

    [no ci] add: template class

---
+++ LupercaliaAdminUtils/ClassTemplate.cs
@@ -0,0 +1,24 @@
+﻿using LupercaliaAdminUtils.model;
+
+namespace LupercaliaAdminUtils;
+
+public class ClassTemplate: IPluginModule
+{
+
+    public string PluginModuleName => "TEMPLATE";
+    
+    private readonly LupercaliaAdminUtils _plugin;
+
+    public ClassTemplate(LupercaliaAdminUtils plugin)
+    {
+        _plugin = plugin;
+    }
+    
+    public void AllPluginsLoaded()
+    {
+    }
+
+    public void UnloadModule()
+    {
+    }
+}
