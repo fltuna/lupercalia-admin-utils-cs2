f90577e188206d209cc92051e8ffaaef3aa0d3eb
faketuna <faketuna.st@gmail.com>
2025-04-11 09:37:26 +0900

    add: implement set team name

--- LupercaliaAdminUtils/LupercaliaAdminUtils.cs
+++ LupercaliaAdminUtils/LupercaliaAdminUtils.cs
@@ -31,5 +31,6 @@ public class LupercaliaAdminUtils: TncssPluginBase
         RegisterModule<UserList>();
         RegisterModule<SetPlayerName>();
         RegisterModule<SetTeamScore>();
+        RegisterModule<SetTeamName>();
     }
 }
