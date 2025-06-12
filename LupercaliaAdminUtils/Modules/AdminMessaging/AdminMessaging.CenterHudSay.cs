88abccd04ec716183c37632f3a766bfabccc1702
fltuna <47306029+fltuna@users.noreply.github.com>
2025-06-12 19:24:43 +0900

    add: implement multiple say features (#63)

---
+++ LupercaliaAdminUtils/Modules/AdminMessaging/AdminMessaging.CenterHudSay.cs
@@ -0,0 +1,64 @@
+﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Translations;
+using CounterStrikeSharp.API.Modules.Admin;
+using CounterStrikeSharp.API.Modules.Commands;
+using CounterStrikeSharp.API.Modules.Commands.Targeting;
+using TNCSSPluginFoundation.Models.Plugin;
+using TNCSSPluginFoundation.Utils.Entity;
+
+namespace LupercaliaAdminUtils.Modules.AdminMessaging;
+
+public sealed partial class AdminMessaging
+{
+    [RequiresPermissions(@"css/generic")]
+    private void CommandCenterHudSay(CCSPlayerController? client, CommandInfo info)
+    {
+        if (info.ArgCount <= 2)
+        {
+            if (client == null)
+            {
+                Server.PrintToConsole(LocalizeString("AdminMessaging.Command.General.Usage", info.ArgByIndex(0).ToLower().Replace("css_", "")));
+            }
+            else
+            {
+                client.PrintToChat(LocalizeWithPluginPrefixForPlayer(client, "AdminMessaging.Command.General.Usage", info.ArgByIndex(0).ToLower().Replace("css_", "")));
+            }
+            return;
+        }
+
+        TargetResult targets = info.GetArgTargetResult(1);
+        
+        if(!targets.Any())
+        {
+            if (client == null)
+            {
+                Server.PrintToConsole(LocalizeString("General.Command.Notification.TargetNotFound"));
+            }
+            else
+            {
+                client.PrintToChat(LocalizeWithPluginPrefixForPlayer(client, "General.Command.Notification.TargetNotFound"));
+            }
+            return;
+        }
+
+        string executorName = PlayerUtil.GetPlayerName(client);
+        bool hasTypedTargets = Target.TargetTypeMap.ContainsKey(info.GetArg(1));
+        string message = GetPlayerMessageFromArgString(info.ArgString);
+
+        if (hasTypedTargets && targets.Count() >= 2)
+        {
+            foreach(CCSPlayerController target in targets) {
+                if(target.IsBot || target.IsHLTV)
+                    continue;
+                
+                target.PrintToCenter(LocalizeStringForPlayer(target, "AdminMessaging.CenterHudSay.Message", executorName, message.ReplaceColorTags()));
+            }
+        }
+        else
+        {
+            CCSPlayerController target = targets.First();
+            target.PrintToCenter(LocalizeStringForPlayer(target, "AdminMessaging.CenterHudSay.Message", executorName, message.ReplaceColorTags()));
+        }
+    }
+}
